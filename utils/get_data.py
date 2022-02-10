e94f4dca619924191d474bf59407966e5150f24e
ppoak <oakery@qq.com>
2022-02-11 00:52:21 +0800

    调整目录结构、更新数据函数
    1. 将因子研究子项目下的utils统一归并到根目录下的utils中
    2. 添加数据函数沪深300指数股票权重
    3. 添加数据函数获取板块信息

--- utils/get_data.py
+++ utils/get_data.py
@@ -71,6 +71,56 @@ def index_market_daily(code: str,
     data.trade_dt = pd.to_datetime(data.trade_dt)
     return data
 
+def index_hs300_close_weight(start: Union[datetime.datetime, datetime.date, str],
+                              end: Union[datetime.datetime, datetime.date, str],
+                              fields: list = None,
+                              conditions: list = None) -> pd.DataFrame:
+    '''get index hs300 close weight in daily frequency
+    -------------------------------------
+
+    start: datetime or date or str, start date in 3 forms
+    end: datetime or date or str, end date in 3 forms
+    fields: list, the field names you want to get
+    conditions: list, a series of conditions like "code = '000001.SZ'" listed in a list
+    '''
+    if fields is None:
+        fields = '*'
+    else:
+        fields = ', '.join(fields)
+    sql = f'select {fields} from index_hs300_close_weight ' + \
+        f'where trade_dt >= "{start}" and trade_dt <= "{end}"'
+    if conditions:
+        conditions = 'and ' + 'and'.join(conditions)
+        sql += conditions
+    data = pd.read_sql(sql, stock_database)
+    data.trade_dt = pd.to_datetime(data.trade_dt)
+    return data
+
+def plate_info(start: Union[datetime.datetime, datetime.date, str],
+               end: Union[datetime.datetime, datetime.date, str],
+               fields: list = None,
+               conditions: list = None) -> pd.DataFrame:
+    '''get plate info in daily frequency
+    -------------------------------------
+
+    start: datetime or date or str, start date in 3 forms
+    end: datetime or date or str, end date in 3 forms
+    fields: list, the field names you want to get
+    conditions: list, a series of conditions like "code = '000001.SZ'" listed in a list
+    '''
+    if fields is None:
+        fields = '*'
+    else:
+        fields = ', '.join(fields)
+    sql = f'select {fields} from plate_info ' + \
+        f'where trade_date >= "{start}" and trade_date <= "{end}"'
+    if conditions:
+        conditions = 'and ' + 'and'.join(conditions)
+        sql += conditions
+    data = pd.read_sql(sql, stock_database)
+    data.trade_date = pd.to_datetime(data.trade_date)
+    return data
+
 if __name__ == "__main__":
-    data = index_market_daily('2021-01-01', '2021-01-05', '000046.SH')
+    data = plate_info('2021-01-01', '2021-01-05')
     print(data)
--- utils/treatment.py
+++ utils/treatment.py
@@ -1,25 +1,13 @@
-#!python -m utils.treatment
 import numpy as np
 
 
 def median_correct(data, n=5):
-    """中位数去极值的方法
+    """deextrme using median
     -------------------------
 
-    传统的去极值方法因为涉及到标准差的计算，由于
-    极值的存在导致标准差已经存在偏差，因此采用中位数去极值的方法，
-    可以产生相对于中位数去极值方法更为稳健的结果
-
-    Parameters:
-    -----------
-
-    data: np.array, 原始数据
-    n: int, 距离中位数的偏差
-
-    Return:
-    ------------
-
-    np.array, 中位数去极值后的结果
+    data: np.array, raw data
+    n: int, n deviations from median
+    return: np.array, result
     """
     md = np.median(data[~np.isnan(data)])
     MAD = np.median(np.abs(data[~np.isnan(data)] - md))
@@ -49,26 +37,19 @@ def fix_odd_percentile(data, n):
 
 
 def deextreme(data, n=None, deextreme_type='median'):
-    """去极值方法
+    """deextreme ways
     ----------------
 
-    Parameters:
-    ----------------
-
-    data: np.array, 原始数据
+    data: np.array, raw data
     n: int, 
-        当使用中位数去极值的方法时，n为数值点距离中位数的距离
-        当使用固定比例去极值时，n必须为以下之一
-            1. 列表形式的n表示保留在列表头尾的两个分位点之间的数据
-            2. 浮点数形式的n表示去掉n比例的头尾数据
-    deextreme_type: str, 必须为一下的一个
-        'median': 中位数去极值
-        'fix_odd': 固定比例去极值
-
-    Return:
-    ------------------
-
-    np.array, 去极值后的数据
+        when using median, n is the number of deviations from median
+        when using fixed odd percentile, n must be one of those:
+            1. the remaining percentiles for head and tail in a list form
+            2. the n percentiles for head and tail in a float form
+    deextreme_type: str, must be one of these:
+        'median': median way
+        'fix_odd': fixed odd way
+    return: np.array, result
     """
     if deextreme_type == 'median':
         if not n:
@@ -90,19 +71,11 @@ def deextreme(data, n=None, deextreme_type='median'):
 
 
 def zscore(data):
-    """z分数标准化方法
+    """z-score standardization
     -------------------------
 
-    zscore = (value - mean) / std
-
-    Parameters:
-    -----------
-
-    data: np.array, 原始数据
-
-    Return: 
-    -----------
-    np.array, 标准化后的数据
+    data: np.array, raw data
+    return: np.array, result
     """
     mean = np.mean(data[~np.isnan(data)])
     std = np.std(data[~np.isnan(data)])
@@ -111,23 +84,15 @@ def zscore(data):
 
 
 def standard(data, standard_type='zscore'):
-    """数据标准化的函数
+    """standardization ways
     -----------------
 
-    为了统一量纲同时方便回归，必须对原始因子数据进行标准化
-
-    Parameters:
-    -----------
-
-    data: np.array, 原始数据
-    standard_type: str, 回归方法必须是一下之一
-        'zscore': z分数标准化方法
-        'weight': 权重标准化方法
-        'rank': 排序标准化法，更加稳健，能够保持因子数据的序关系
-
-    Return:
-    -----------
-    np.array, 标准化后的因子数据
+    data: np.array, raw data
+    standard_type: str, standardization type must be one of these:
+        'zscore': z-score standardization
+        'weight': weight standardization
+        'rank': rank standardization
+    return: np.array, 标准化后的因子数据
     """
     if standard_type == 'zscore':
         new_data = zscore(data)
@@ -138,24 +103,16 @@ def standard(data, standard_type='zscore'):
         # TODO: 排序标准化待完成
         pass
     else:
-        raise ValueError(
-            "type should be chosen in ['zscore', 'weight', 'rank']")
+        raise ValueError("type should be chosen in ['zscore', 'weight', 'rank']")
     return new_data
 
 
 def missing_fill(data: np.array) -> np.array:
-    """缺失值填充方法
+    """missing fill ways
     ---------------------
 
-    Parameters:
-    -----------
-
-    data: np.array, 原始数据
-
-    Return:
-    -----------
-
-    np.array, 将缺失值填充为0后的数据
+    data: np.array, raw data
+    return: np.array, missing data filled with 0
     """
     new_data = data.copy()
     new_data[np.isnan(new_data)] = 0
