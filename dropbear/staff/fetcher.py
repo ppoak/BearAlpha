6a17fb6b005b1e581e3986a78c61c8cc09f94541
ppoak <oakery@qq.com>
2022-04-20 18:46:02 +0800

    修复db.read_excel最简单调用情况下无法设定默认index_col为0的情况

--- dropbear/staff/fetcher.py
+++ dropbear/staff/fetcher.py
@@ -17,7 +17,8 @@ def read_excel(path, index_col=None, parse_dates=True,
     indicators=False, assets=False, datetimes=False, **kwargs):
     '''Read excel file and return a dataframe of a panelframe'''
     if indicators or assets or datetimes:
-        index_col = 0
+        if index_col is None:
+            index_col = 0
         pf = pd.read_excel(path, index_col=index_col, parse_dates=parse_dates, sheet_name=None, **kwargs)
         if indicators and not assets and not datetimes:
             return PanelFrame(indicators=pf)
@@ -33,6 +34,8 @@ def read_excel(path, index_col=None, parse_dates=True,
         return PanelFrame(dataframe=pf)
     
     else:
+        if index_col is None:
+            index_col = 0
         return pd.read_excel(path, index_col=index_col, parse_dates=parse_dates, **kwargs)
             
                 
